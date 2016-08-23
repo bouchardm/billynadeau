16a8480b1ab07e6cc865cda101d1e10d6cdee8c4
Marc-Antoine Bouchard M <marcantoine.bouchardm@gmail.com>
2016-08-22 23:17:09 -0400

    lazyinstall + auth

---
+++ lazyinstall.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ ! -d vendor ]; then
+    composer install
+fi
+
+if [ ! -d node_modules ]; then
+    npm install
+    gulp
+fi
+
+if [ ! -f .env ]; then
+    cp .env.example .env
+    php artisan key:generate
+fi
+
+if [ ! -f database.sqlite ]; then
+    touch database/database.sqlite
+    php artisan migrate
+fi
